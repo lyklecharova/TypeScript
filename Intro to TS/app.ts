815a218dde34a129fcc8e1bd9e2eb218bf2c5e10
lyklecharova <lklecharova9@gmail.com>
2024-04-04 11:31:58 +0300

    Update app.ts

--- Intro to TS/app.ts
+++ Intro to TS/app.ts
@@ -45,7 +45,6 @@
 
 // console.log(arr5);
 
-
 // // Tuples     key     value
 // let tuple1: [number, string] = [123, "Hi"];
 // let tuple2: [boolean, object] = [true, {name: "Peshho"}];
@@ -54,4 +53,59 @@
 // console.log(tuple1);
 // console.log(tuple2);
 
-// Enums
+// Enum
+// enum DaysOfWeek{
+//     // M = 1,
+//     // T,
+//     // W,
+//     // Th,
+//     // F,
+
+//     M,
+//     T,
+//     W,
+//     Th,
+//     F,
+// }
+
+// const currentDay = 1;
+
+// if(currentDay === DaysOfWeek.M){
+//     console.log('Is Monday')
+// }
+
+// // Any, Unknown -> "type safe"
+// let a:unknown;
+// a = 5;
+// a = {name: "Pesho"};
+// a = true;
+// console.log(a);
+
+// // Void
+// function myPrint(input: string):void {
+//     console.log(input);
+//     // doesn't gas retunr value!
+// }
+
+// function myPrint2(input:string):string{
+//     return `[from print function] : ${input}`;
+// }
+// myPrint('Hello!')
+// console.log(myPrint2('Hello!'));
+
+
+// // Optional params
+// function optionalParams(param1 = 5, param2?:string):void{
+//     console.log({param1, param2});
+    
+// }
+// // from left to right ->
+// optionalParams(1);
+
+// // Default arguments
+// function optionalParams2(param1 = 5, param2:"Hi from default"):void{
+//     console.log({param1, param2});
+    
+// }
+
+
