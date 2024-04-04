37b824366840d8bc8014b0eb32287f9fedb48610
lyklecharova <lklecharova9@gmail.com>
2024-04-04 08:04:07 +0300

    Create app.js

---
+++ Intro to TS/app.js
@@ -0,0 +1,9 @@
+const a = 5; // number
+a = ' Hello'; // string
+a = { name: 'Pesho', country: 'Bulgaria' }; // object
+console.log(a);
+
+
+// let arrOfNumbers = [1, 2, 3, 4];
+// arrOfNumbers = true;
+// arrOfNumbers = ['a', 'b', 'c'];
