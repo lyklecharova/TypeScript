0a068da48ca5537109431608c9fa47b51e5934ff
lyklecharova <lklecharova9@gmail.com>
2024-04-04 12:11:50 +0300

    Create Largest Number.ts

---
+++ Intro to TS/lab/problem2/Largest Number.ts
@@ -0,0 +1,10 @@
+function largestNumber(num1: number, num2: number, num3: number): void {
+    const largest = Math.max(num1, num2, num3);
+    console.log(`The largest number is ${largest}.`);
+}
+
+// Example usage:
+largestNumber(5, -3, 16);
+largestNumber(-3, -5, -22.5);
+
+
