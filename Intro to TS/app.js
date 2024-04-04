a20d1f0ed755cddb4c3ddcf63bb01e659bccc507
lyklecharova <lklecharova9@gmail.com>
2024-04-04 11:07:01 +0300

    Update app.js

--- Intro to TS/app.js
+++ Intro to TS/app.js
@@ -8,9 +8,36 @@
 // let hex: number = 7e4;
 // let binary: number = 10001101;
 // console.log(binary);
-// Booleans
-var hasAuth = true;
-var isGreater = 5 > 7;
-var hasTwo = [1, 2, 3, 4].some(function (e) { return e === 22; });
-var isBoo = 123;
-console.log(isBoo);
+// // Booleans
+// let hasAuth: boolean = true;
+// const isGreater: boolean = 5 >7;
+// const hasTwo = [1,2,3,4].some(e=> e ===22);
+// const isBoo:boolean = 123;
+// console.log(isBoo);
+// // Symbols
+// let sym1 = Symbol('key');
+// let symb2 = Symbol('key');
+// console.log(sym1);
+// // Undefined
+// let user: Object | undefined;
+// let person: Object | null = null;
+// console.log({ user });
+// console.log({ person });
+// setTimeout(() => {
+//     user = {name: "user123"};
+//     console.log({ user });
+//     person = {name: "Pesho"};
+//     console.log({ person });
+// }, 3000);
+// // Arrays
+// const arr1: number[] = [1, 2, 3, 4];
+// const arr2: Array<number> = [2, 3, 4, 5];
+// const arr3: string[] = ['Hello', 'its', 'me'];
+// const arr4: boolean[] = [true, true, false, false];
+// const arr5: any[] = [1, 'goole', false, {name: "Peshho"}];
+// console.log(arr5);
+// Tuples
+var tuple1 = [123, "Hi"];
+var tuple2 = [true, { name: "Peshho" }];
+console.log(tuple1);
+console.log(tuple2);
