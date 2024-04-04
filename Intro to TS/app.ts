a020e4872763dd9d24c8717b1ccd2eb00cd377db
lyklecharova <lklecharova9@gmail.com>
2024-04-04 11:07:03 +0300

    Update app.ts

--- Intro to TS/app.ts
+++ Intro to TS/app.ts
@@ -16,3 +16,42 @@
 // const hasTwo = [1,2,3,4].some(e=> e ===22);
 // const isBoo:boolean = 123;
 // console.log(isBoo);
+
+// // Symbols
+// let sym1 = Symbol('key');
+// let symb2 = Symbol('key');
+// console.log(sym1);
+
+// // Undefined
+// let user: Object | undefined;
+// let person: Object | null = null;
+
+// console.log({ user });
+// console.log({ person });
+// setTimeout(() => {
+//     user = {name: "user123"};
+//     console.log({ user });
+
+//     person = {name: "Pesho"};
+//     console.log({ person });
+// }, 3000);
+
+// // Arrays
+// const arr1: number[] = [1, 2, 3, 4];
+// const arr2: Array<number> = [2, 3, 4, 5];
+// const arr3: string[] = ['Hello', 'its', 'me'];
+// const arr4: boolean[] = [true, true, false, false];
+// const arr5: any[] = [1, 'goole', false, {name: "Peshho"}];
+
+// console.log(arr5);
+
+
+// // Tuples     key     value
+// let tuple1: [number, string] = [123, "Hi"];
+// let tuple2: [boolean, object] = [true, {name: "Peshho"}];
+// let tupleObjKey: [string, object] = ["user", {name: "User123"}];
+
+// console.log(tuple1);
+// console.log(tuple2);
+
+// Enums
