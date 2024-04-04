0733107e55f9b21168bfd006e70d712d47de7922
lyklecharova <lklecharova9@gmail.com>
2024-04-04 10:21:04 +0300

    Create app.js

---
+++ Intro to TS/app.js
@@ -0,0 +1,10 @@
+// // String
+// let str: string = "Hello";
+// str = `Hello, ${123}`;
+// // str = true; //invalid
+// console.log(str);
+// Number
+var a = 5;
+var hex = 7e4;
+var binary = 10001101;
+console.log(binary);
