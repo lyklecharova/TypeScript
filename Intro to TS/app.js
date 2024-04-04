170de48f20caccf27932fefc3dffe99a4608573e
lyklecharova <lklecharova9@gmail.com>
2024-04-04 08:50:01 +0300

    Update app.js

--- Intro to TS/app.js
+++ Intro to TS/app.js
@@ -1,9 +1,2 @@
-const a = 5; // number
-a = ' Hello'; // string
-a = { name: 'Pesho', country: 'Bulgaria' }; // object
-console.log(a);
-
-
-// let arrOfNumbers = [1, 2, 3, 4];
-// arrOfNumbers = true;
-// arrOfNumbers = ['a', 'b', 'c'];
+const greeting = "Hello";
+console.log(greeting);
