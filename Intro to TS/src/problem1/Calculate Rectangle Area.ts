b4918af2c5978931117a162eac3e711a8ac91650
lyklecharova <lklecharova9@gmail.com>
2024-04-04 12:34:41 +0300

    Create Calculate Rectangle Area.ts

---
+++ Intro to TS/src/problem1/Calculate Rectangle Area.ts
@@ -0,0 +1,8 @@
+function calculateRectangleArea(length: number,width:number):number {
+    return length * width;
+}
+
+const length = 5;
+const width = 7;
+const area = calculateRectangleArea(length, width);
+console.log(area);
