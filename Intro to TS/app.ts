78db54ba73ba23a9c7b8feb66f16026eb21f0fc3
lyklecharova <lklecharova9@gmail.com>
2024-04-04 10:27:15 +0300

    Update app.ts

--- Intro to TS/app.ts
+++ Intro to TS/app.ts
@@ -9,3 +9,10 @@
 // let hex: number = 7e4;
 // let binary: number = 10001101;
 // console.log(binary);
+
+// // Booleans
+// let hasAuth: boolean = true;
+// const isGreater: boolean = 5 >7;
+// const hasTwo = [1,2,3,4].some(e=> e ===22);
+// const isBoo:boolean = 123;
+// console.log(isBoo);
