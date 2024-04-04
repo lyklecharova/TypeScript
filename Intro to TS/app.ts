fb476ecf5d1f7687cd6f5c11d37f9a5937ec94b8
lyklecharova <lklecharova9@gmail.com>
2024-04-04 10:21:06 +0300

    Create app.ts

---
+++ Intro to TS/app.ts
@@ -0,0 +1,11 @@
+// // String
+// let str: string = "Hello";
+// str = `Hello, ${123}`;
+// // str = true; //invalid
+// console.log(str);
+
+// Number
+// let a: number = 5;
+// let hex: number = 7e4;
+// let binary: number = 10001101;
+// console.log(binary);
