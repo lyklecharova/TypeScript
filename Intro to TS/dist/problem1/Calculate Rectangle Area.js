368d4e4a5ba6d08174a5b7f1b8ca3996a8d7a422
lyklecharova <lklecharova9@gmail.com>
2024-04-04 12:08:43 +0300

    Create Calculate Rectangle Area.js

---
+++ Intro to TS/dist/problem1/Calculate Rectangle Area.js
@@ -0,0 +1,7 @@
+function calculateRectangleArea(length, width) {
+    return length * width;
+}
+const length = 5;
+const width = 7;
+const area = calculateRectangleArea(length, width);
+console.log(area);
