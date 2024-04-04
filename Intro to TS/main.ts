c20b6cfe82d6f683442d55633ce8d47b54fc5075
lyklecharova <lklecharova9@gmail.com>
2024-04-04 08:04:12 +0300

    Create main.ts

---
+++ Intro to TS/main.ts
@@ -0,0 +1,17 @@
+// let b = 5;
+// b = 'Hello';
+// b = {name: 'Pesho'};
+// console.log(b);
+
+// let arr: number[] = [1, 2, 3, 4];
+// arr = false ;
+// arr = [true, 1, 'c'];
+
+var a = 5;
+a = 'string';
+
+console.log(a + ' ' + a);
+
+
+// TypeScript is superset of JavaScript
+// ES6 is superset of ES5
