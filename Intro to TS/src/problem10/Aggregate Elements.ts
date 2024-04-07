7ca06ed518f9eba9c1e1a48c5676c9762c0cc6f9
lyklecharova <lklecharova9@gmail.com>
2024-04-07 07:11:34 +0300

    Create Aggregate Elements.ts

---
+++ Intro to TS/src/problem10/Aggregate Elements.ts
@@ -0,0 +1,21 @@
+function aggregateElements(arr: number[]): number {
+    return arr.reduce((acc, curr) => acc + curr, 0);
+}
+
+function sumInverse(arr: number[]): number {
+    return arr.reduce((acc, curr) => acc + 1 / curr, 0);
+}
+
+function concat(arr: number[]): string {
+    return arr.join("");
+}
+
+const arr1 = [1, 2, 3];
+console.log(aggregateElements(arr1)); // Output: 6
+console.log(sumInverse(arr1)); // Output: 1.8333333333333333
+console.log(concat(arr1)); // Output: 123
+
+const arr2 = [2, 4, 8, 16];
+console.log(aggregateElements(arr2)); // Output: 30
+console.log(sumInverse(arr2)); // Output: 0.9375
+console.log(concat(arr2)); // Output: 24816
